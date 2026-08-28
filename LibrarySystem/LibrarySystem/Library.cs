61e6240544e08b70a9787448ad0a217539934a18
Nicolai Nelbom <79920825+nelbom1234@users.noreply.github.com>
2026-08-28 08:56:07 +0200

    create library

---
+++ LibrarySystem/LibrarySystem/Library.cs
@@ -0,0 +1,17 @@
+﻿namespace Lesson02.Library;
+
+public class Library
+{
+    private List<Book> _bookCollection;
+
+    public Library(List<Book> bookCollection)
+    {
+        _bookCollection = bookCollection;
+    }
+
+    public List<Book> BookCollection
+    {
+        get => this._bookCollection; 
+        set => this._bookCollection = value;
+    }
+}
