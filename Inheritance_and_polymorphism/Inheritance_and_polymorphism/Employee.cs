0c9a835984954e130a0c29dec03f03da873cb7c8
Nicolai Nelbom <79920825+nelbom1234@users.noreply.github.com>
2026-08-31 08:13:01 +0200

    add employee base class

---
+++ Inheritance_and_polymorphism/Inheritance_and_polymorphism/Employee.cs
@@ -0,0 +1,30 @@
+﻿namespace Inheritance_and_polymorphism;
+
+public class Employee
+{
+    public Employee(
+        string name,
+        string employeeId,
+        DateTime hireDate)
+    {
+        Name = name;
+        EmployeeId = employeeId;
+        HireDate = hireDate;
+    }
+    
+    public string Name { get; private set; }
+    
+    public string EmployeeId { get; private set; }
+    
+    public DateTime HireDate { get; init; }
+
+    public virtual decimal CalculateSalary()
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual string Description()
+    {
+        return $"{Name} tjener {CalculateSalary()} kr. om måneden.";
+    }
+}
